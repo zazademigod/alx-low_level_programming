c063f4ae72fedc68d0177905008701e8198a9ca8
david <david.ndubuidi@gmail.com>
2023-03-01 04:30:30 -0800

    adding the updated c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*this is a program to print a message to the console using puts as the printer*/
+/*this is a program to print message to the console using puts*/
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
