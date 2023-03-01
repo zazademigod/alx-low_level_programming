1f53b2d0541b17cee485d13a44134379c3a7c7c7
david <david.ndubuidi@gmail.com>
2023-03-01 04:24:03 -0800

    adding the updated c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
-/*this is a program to print a message to the console using puts with the 
- * with the first single quote appearing after using the (\) so that the fiel ignores it*/
 #include <stdio.h>
-
+/*this is a program to print a message to the console using puts as the printer*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
