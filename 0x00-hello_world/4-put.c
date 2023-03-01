a01b5bf6dd5e91630c64c2cb71bbcb4f4441caf8
david <david.ndubuidi@gmail.com>
2023-03-01 03:50:26 -0800

    adding first c file

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,8 @@
+/*this is a program to print a message to the console using puts */
+#include <stdio.h>
+
+int main(void)
+{
+	puts('"Programming is like building a multilingual puzzle\n');
+	return (0);
+}
