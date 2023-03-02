de1e7d2f45829ae8d530aa0890583ae8d5b4444f
david <david.ndubuidi@gmail.com>
2023-03-02 06:12:51 -0800

    modifying file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/*a program to print a message to the terminal*/
+/**
+ * main - Entry point of the program
+ *
+ * Descripton: Prints the text"with proper grammar, but the outcome is a piece of art,"
+ * 	followed by a new line
+ *
+ * 	Return: Alwaya 0 (success)*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
