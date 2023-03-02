0bac59d624644bf4959c038c178a08da50263400
david <david.ndubuidi@gmail.com>
2023-03-02 07:20:24 -0800

    mod compiled 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,9 @@
 /** 
  * main - Entry point of the program
  *
- * Descripton: Prints the text"with proper grammar, but the outcome is a piece of art,"
- * 	followed by a new line
- *
  * 	Return: Alwaya 0 (success)
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
