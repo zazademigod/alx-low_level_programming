14b885165ef023bbf788d03f86ebd410e9a29a06
david <david.ndubuidi@gmail.com>
2023-03-02 05:58:02 -0800

    update to file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,57 +7,6 @@
  */
 int main(void)
 {
-	putchar('"');
-	putchar('P');
-	putchar('r');
-	putchar('o');
-	putchar('g');
-	putchar('r');
-	putchar('a');
-	putchar('m');
-	putchar('m');
-	putchar('i');
-	putchar('n');
-	putchar('g');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('l');
-	putchar('i');
-	putchar('k');
-	putchar('e');
-	putchar(' ');
-	putchar('b');
-	putchar('u');
-	putchar('i');
-	putchar('l');
-	putchar('d');
-	putchar('i');
-	putchar('n');
-	putchar('g');
-	putchar(' ');
-	putchar('a');
-	putchar(' ');
-	putchar('m');
-	putchar('u');
-	putchar('l');
-	putchar('t');
-	putchar('i');
-	putchar('l');
-	putchar('i');
-	putchar('n');
-	putchar('g');
-	putchar('u');
-	putchar('a');
-	putchar('l');
-	putchar(' ');
-	putchar('p');
-	putchar('u');
-	putchar('z');
-	putchar('z');
-	putchar('l');
-	putchar('e');
-	putchar('\n');
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
