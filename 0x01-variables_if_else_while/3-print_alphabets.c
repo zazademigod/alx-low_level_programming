7a4ede191f65bdbdb0bc0667fc76fd8b6d391601
david <david.ndubuidi@gmail.com>
2023-03-04 07:11:38 -0800

    mod for 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
-
+/**
+ * main - this is the entry o fthe program
+ *
+ * Description: Prints the alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
+
 {
 	char c = 'a';
+
 	char C = 'A';
+
 	while (c <= 'z')
 	{
+
 		putchar(c);
+
 		c++;
+
 	}
 
 	while (C <= 'Z')
 	{
+
 		putchar(C);
+
 		C++;
+
 	}
+
 	putchar('\n');
+
 	return (0);
 }
 
