cee00730a0cd1a8ab5b567f43721c6eaeb38e600
david <david.ndubuidi@gmail.com>
2023-03-04 06:56:31 -0800

    mod for 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* *
+/**
  * main - Entry point of the program
+ *
  * Description: generate output based on random number
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
