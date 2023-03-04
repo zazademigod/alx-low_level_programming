4956d91d6698418291162d589bfd7dbfa7c70a92
david <david.ndubuidi@gmail.com>
2023-03-04 09:09:56 -0800

    adding file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,43 @@ int main(void)
 
 	for (c = 0; c < 16;)
 	{
-		putchar (c + '0');
+		if (c < 10)
+		{
+			putchar (c + '0');
+		}
+
+		else
+		{
+			if (i == 10)
+			{
+				putchar ('a');
+			}
+
+			else if (i == 11)
+			{
+				putchar ('b');
+			}
+
+			else if (i == 12)
+			{
+				putchar ('c');
+			}
+
+			else if (i == 13)
+			{
+				putchar ('d');
+			}
+
+			else if (i == 14)
+			{
+				putchar ('e');
+			}
+
+			else if (i == 15)
+			{
+				putchar ('f');
+			}
+		}
 
 		c++;
 	}
