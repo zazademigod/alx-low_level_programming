bc5787f67898661d9846227492b262248e0477d1
david <david.ndubuidi@gmail.com>
2023-03-03 20:45:59 -0800

    mods for variables projesct

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point into program
+ * Description: This is a program that prints the alphabet in lower case
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
+	
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
+
