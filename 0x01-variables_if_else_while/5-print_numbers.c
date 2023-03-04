8ffdf33e925a0f5020ec77072a762de07ebc7926
david <david.ndubuidi@gmail.com>
2023-03-04 08:02:07 -0800

    mod for 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry to the program
+ *
+ * Descrption: to print numbers of base 10
+ *
+ * Return: Always 0 for successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <=9;)
+	{
+		printf("%d\n", c);
+
+		c++;
+	}
+
+	return (0);
+}
