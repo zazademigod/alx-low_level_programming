1f664ad1da5e54299d0aff8bbc1167ee51c95408
david <david.ndubuidi@gmail.com>
2023-03-05 11:22:57 -0800

    adding 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry to the program
+ * Description: to print _putchar
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#this is a readme for my third directory hwile leatning c 
+its for tests on c functions and programs
