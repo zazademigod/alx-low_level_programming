82d5b0f7f8cb5a8cfa97b2a4c49350f99c1f0b51
david <david.ndubuidi@gmail.com>
2023-03-04 17:17:57 -0800

    adding 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main -entry of thr program
+ *
+ * Description: to print two two digit numbers
+ *
+ * Return: Always 0 for sucessful
+ */
+int main(void)
+{
+	int a;
+
+	int b;
+
+	for (a = 0; a < 100; a++)
+	{
+
+		for (b = a + 1; b <100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a != 98 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
