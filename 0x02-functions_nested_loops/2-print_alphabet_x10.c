86a90b2883e415da7af850e1f4f34b2ff7058c2a
david <david.ndubuidi@gmail.com>
2023-03-05 12:16:07 -0800

    adding 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabets
+ * ten times
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+
+	char b;
+
+	for (a =1; a <= 0;)
+	{
+		for (b = 'a'; b <= 'z';)
+		{
+			_putchar(b);
+			b++;
+		}
+		a++;
+	}
+	_putchar('\n');
+}
